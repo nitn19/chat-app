0157796f65d9478d4973d22d7e39b2793366489f
nitin19 <batranitin44@gmail.com>
2022-06-25 13:23:55 +0530

    intialized firebase project

---
+++ Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDH4zikHliv5YwuU0oXw_nMZE49kh-jlzI',
+  authDomain: 'chat-web-app-554e1.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-554e1-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-554e1',
+  storageBucket: 'chat-web-app-554e1.appspot.com',
+  messagingSenderId: '1038433330263',
+  appId: '1:1038433330263:web:e7a8a97a00985e05c0ef12',
+};
+
+const app = firebase.initializeApp(config);
