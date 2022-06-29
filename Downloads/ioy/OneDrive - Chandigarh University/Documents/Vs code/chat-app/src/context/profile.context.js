7b895d5261dad63a1e259078ddcc86f274debc6d
nitin19 <batranitin44@gmail.com>
2022-06-29 18:19:48 +0530

    add real time precence listener for a user

--- Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/context/profile.context.js
+++ Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/context/profile.context.js
@@ -1,6 +1,17 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import firebase from 'firebase/app';
 import { auth, database } from '../misc/firebase';
+
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 const ProfileContext = createContext();
 
@@ -10,8 +21,10 @@ export const ProfileProvider = ({ children }) => {
 
   useEffect(() => {
     let userRef;
+    let userStatusRef;
     const authUnsub = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
         userRef = database.ref(`/profiles/${authObj.uid}`);
         userRef.on('value', snap => {
           const { name, createdAt, avatar } = snap.val();
@@ -27,10 +40,27 @@ export const ProfileProvider = ({ children }) => {
           setProfile(data);
           setIsLoading(false);
         });
+
+        database.ref('.info/connected').on('value', snapshot => {
+          if (snapshot.val() === false) {
+            return;
+          }
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              userStatusRef.set(isOnlineForDatabase);
+            });
+        });
       } else {
         if (userRef) {
           userRef.off();
         }
+
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
+        database.ref('.info/connected').off();
         setProfile(null);
         setIsLoading(false);
       }
@@ -38,8 +68,15 @@ export const ProfileProvider = ({ children }) => {
 
     return () => {
       authUnsub();
+
+      database.ref('.info/connected').off();
+
       if (userRef) {
         userRef.off();
+      }
+
+      if (userStatusRef) {
+        userStatusRef.off();
       }
     };
   }, []);
