8b6c4c2041e9bbb108308baf7c64fd575e8be83b
nitin19 <batranitin44@gmail.com>
2022-06-29 12:11:20 +0530

    update all user references if data is updated

--- Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/components/dashboard/index.js
+++ Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/components/dashboard/index.js
@@ -6,16 +6,21 @@ import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
 import AvatarUploadBtn from './AvatarUploadBtn';
 import ProvideBlocker from './ProviderBlock';
+import { getUserUpdates } from '../../misc/helpers';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async newData => {
-    const userNicknameRef = database
-      .ref(`/profiles/${profile.uid}`)
-      .child('name');
     try {
-      await userNicknameRef.set(newData);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+      database.ref().update(updates);
+
       Alert.success('Nickname has been updated', 4000);
     } catch (err) {
       Alert.error(err.messge, 4000);
