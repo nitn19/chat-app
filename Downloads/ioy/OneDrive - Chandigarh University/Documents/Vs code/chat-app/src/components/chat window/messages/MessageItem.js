58e2852adf8d443b0ab7bb4d876328d55886eacf
nitin19 <batranitin44@gmail.com>
2022-06-30 11:26:43 +0530

    added button to grant or remove admin

--- Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/components/chat window/messages/MessageItem.js
+++ Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/components/chat window/messages/MessageItem.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { memo } from 'react';
+import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
 
-const MessageItem = ({ message }) => {
+const MessageItem = ({ message, handleAdmin }) => {
   const { author, createdAt, text } = message;
+
+  const isAdmin = useCurrentRoom(v => v.isAdmin);
+  const admins = useCurrentRoom(v => v.admins);
+
+  const isMsgAuthorAdmin = admins.includes(author.uid);
+  const isAuthor = auth.currentUser.uid === author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
 
   return (
     <li className="padded mb-1">
@@ -21,17 +31,25 @@ const MessageItem = ({ message }) => {
           profile={author}
           appearance="link"
           className="p-0 ml-1 text-black"
-        />
+        >
+          {canGrantAdmin && (
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+              {isMsgAuthorAdmin
+                ? 'Remove admin permision'
+                : 'Give admin in this room'}
+            </Button>
+          )}
+        </ProfileInfoBtnModal>
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
         />
       </div>
       <div>
-        <span className="word-breal-all">{text}</span>
+        <span className="word-break-all">{text}</span>
       </div>
     </li>
   );
 };
 
-export default MessageItem;
+export default memo(MessageItem);
