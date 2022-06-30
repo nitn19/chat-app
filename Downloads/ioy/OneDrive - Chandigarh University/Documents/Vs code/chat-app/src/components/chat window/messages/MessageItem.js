54853dd4275000ec27451697903a6c4e6a9d857e
nitin19 <batranitin44@gmail.com>
2022-06-30 16:54:29 +0530

    handle delete message operation

--- Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/components/chat window/messages/MessageItem.js
+++ Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/components/chat window/messages/MessageItem.js
@@ -10,7 +10,7 @@ import { auth } from '../../../misc/firebase';
 import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
 import IconBtnControl from './IconBtnControl';
 
-const MessageItem = ({ message, handleAdmin, handleLike }) => {
+const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
   const { author, createdAt, text, likes, likeCount } = message;
 
   const [selfRef, isHoverred] = useHover();
@@ -66,6 +66,14 @@ const MessageItem = ({ message, handleAdmin, handleLike }) => {
           onClick={() => handleLike(message.id)}
           badgeContent={likeCount}
         />
+        {isAuthor && (
+          <IconBtnControl
+            isVisible={canShowIcons}
+            iconName="close"
+            tooltip="Delete this message"
+            onClick={() => handleDelete(message.id)}
+          />
+        )}
       </div>
       <div>
         <span className="word-break-all">{text}</span>
--- Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/components/chat window/messages/index.js
+++ Downloads/ioy/OneDrive - Chandigarh University/Documents/Vs code/chat-app/src/components/chat window/messages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
@@ -77,6 +78,35 @@ const Messages = () => {
     Alert.info(alertMsg, 4000);
   }, []);
 
+  const handleDelete = useCallback(
+    async msgId => {
+      if (!window.confirm('Delete this message?')) {
+        return;
+      }
+      const isLast = messages[messages.length - 1].id === msgId;
+      const updates = {};
+      updates[`/messages/${msgId}`] = null;
+
+      if (isLast && messages.length > 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = {
+          ...messages[messages.length - 2],
+          msgId: messages[messages.length - 2].id,
+        };
+      }
+
+      if (isLast && messages.length === 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = null;
+      }
+      try {
+        await database.ref().update(updates);
+        Alert.info('Messages has been deleted', 4000);
+      } catch (err) {
+        Alert.error(err.message, 4000);
+      }
+    },
+    [chatId, messages]
+  );
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No messages yet</li>}
@@ -87,6 +117,7 @@ const Messages = () => {
             message={msg}
             handleAdmin={handleAdmin}
             handleLike={handleLike}
+            handleDelete={handleDelete}
           />
         ))}
     </ul>
